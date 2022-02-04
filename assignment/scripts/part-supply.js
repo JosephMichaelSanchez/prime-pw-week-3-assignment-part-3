f08e8c3e1f28f4a10446f0518d43b6866c6b891d
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-02-04 15:04:26 -0600

    Andwered Q 7

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -45,7 +45,17 @@ for (let x=0; x<supplyChanges.length; x++){
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
-
+for(transfer of supplyChanges){
+  if(transfer>0){
+    console.log('Added ' + transfer + ' parts.')
+  }
+  if(transfer===0){
+    console.log('No change.')
+  }
+  if(transfer<0){
+    console.log('Removed ' + -transfer + ' parts.')
+  }
+}
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
