46d01a789d71bf6f1160f796426dee4c9b2d1a64
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-02-06 11:01:48 -0600

    Added background image, changed font.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -74,14 +74,11 @@ console.log('9. Filling boxes with a "while" loop');
 let parts=572
 let boxes=0
 while (parts>=7){
-  console.log('Putting 7 parts in a box...');
+  console.log('Filling boxes with 7 parts each...');
   parts-=7;
   boxes++;
   if (parts<7){
     console.log('We filled ' + boxes + ' boxes.')
     console.log('There are ' + parts + ' parts remaining.');
-  }
-  else if (parts===0){
-    console.log('We have filled ' + boxes + ' boxes.')
   }
 }
